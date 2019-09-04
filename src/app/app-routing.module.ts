f77f81c218aa078c72dfb5a84fb2637f7e396ccd
Bradley Hazelton <bradley@thehazeltons.ca>
2019-09-03 21:17:03 -0600

    Added skater to routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,11 @@
+import { SkatersComponent } from './skaters/skaters.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'skaters', component: SkatersComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
